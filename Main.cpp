6b2de264af48032ccb49d6612ee22582325305a6
kousuke1902 <kousuke1902love@gmail.com>
2023-05-22 15:50:04 +0900

    make Project
    Visual Studioのプロジェクトを作成

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.10
+
+void Main()
+{
+	// 背景の色を設定する | Set the background color
+	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
+
+
+
+	while (System::Update())
+	{
+
+
+
+	}
+}
+
