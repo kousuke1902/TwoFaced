99cc5cbaa1d25235396c83c8f917abc3da30f6a1
kousuke1902 <kousuke1902love@gmail.com>
2024-01-27 12:50:44 +0900

    VS並びにOpenSiv3Dでの正式な更新とアップデート
    PCを変えたのでファイルパス位置も再調整

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,4 @@
-﻿#include <Siv3D.hpp> // OpenSiv3D v0.6.10
+﻿#include <Siv3D.hpp>
 #include "SceneData.hpp"
 #include "Stage.hpp"
 #include "Result.hpp"
--- Result.hpp
+++ Result.hpp
@@ -5,11 +5,14 @@
 class Result : public App::Scene
 {
 
+private:
+	Font font;
+
 public:
 
 	Result(const InitData& init): IScene{ init }
 	{
-
+		font = Font{ FontMethod::MSDF , 48 };
 	}
 
 	// 更新関数
@@ -23,8 +26,9 @@ public:
 	// 描画関数
 	void draw() const override
 	{
+		ClearPrint();
 		Print << getData().ResultLife;
-		FontAsset(U"ScoreFont")(U"Score: {}"_fmt(getData().ResultLife)).draw(40, 40);
+		font(U"Score: {}"_fmt(getData().ResultLife)).drawAt(300, 40);
 
 	}
 
